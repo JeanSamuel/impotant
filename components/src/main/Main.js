e5acce960d8d4fb3d3437b8521aa46f14aa0ee0d
toavina <toavinar@outlook.com>
2017-07-11 17:09:05 +0300

    second commit

--- components/src/main/Main.js
+++ components/src/main/Main.js
@@ -12,25 +12,38 @@ export default class Main extends Component {
         }
     }
 
-  state = {
-    text: 'Toavina - 20000',
-  };
- 
+  constructor(props){
+    super(props)
+    this.state = {
+      staticText : 'vola:15478965&num:',
+      amount : '0'
+    }
+  }
+
+  setAmount(amount){
+    if(amount == ''){
+      this.state.amount = '0'
+    }
+    this.setState({amount})
+
+  }
+
   render() {
     return (
       <View style = {Style.view}>
         
         <View style = {Style.qrCode}>
             <QRCode
-            value={this.state.text}
+            value={this.state.staticText + this.state.amount}
             size={200}
             bgColor='#000'
             fgColor='white'/>
         </View>
         <TextInput
+            keyboardType = {'numeric'}
             style = {Style.input}
-          onChangeText={(text) => this.setState({text: text})}
-          value={this.state.text}
+            onChangeText={(text) => this.setAmount(text)}
+            value={this.state.amount}
         />
         
       </View>
