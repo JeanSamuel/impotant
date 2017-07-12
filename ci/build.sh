3446efc2d5003a2956ace1dd232d381290e46380
toavina <toavinar@outlook.com>
2017-07-12 16:46:18 +0300

    build.sh operationnel

--- ci/build.sh
+++ ci/build.sh
@@ -12,10 +12,15 @@ npm install
 react-native upgrade
 
 # echo "début du lancement" for android greater than 5.0.0
-if [ `uname -o` == 'Msys' ]; then
-    react-native run-android
-else
+if [ `uname -o` == 'darwin' ]; then
     react-native run-ios
+else
+    mkdir -p android/app/src/main/assets
+    touch android/app/src/main/assets/index.android.bundle
+    react-native run-android
+    cd ../ && mkdir -p app/ios app/android
+    cp -R build/android/app/build/outputs/apk/ app/android 
+
 fi
 
 
