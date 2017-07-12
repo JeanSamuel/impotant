9f49403d0f6970b8956f297cff2c6f35b1e1a0ca
toavina <toavinar@outlook.com>
2017-07-12 15:52:08 +0300

    with build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,21 @@
 
 cd ../
+# echo "début du copie"
 mkdir -p output/build
 cp -r src/* output/build
 cd output/build
-npm install
-react-native run-android
 
-exit -1
+# echo "début d'installation"
+npm install
+
+# installation des packages natives
+react-native upgrade
+
+# echo "début du lancement" for android greater than 5.0.0
+if [ `uname -o` == 'Msys' ]; then
+    react-native run-android
+else
+    react-native run-ios
+fi
+
+
