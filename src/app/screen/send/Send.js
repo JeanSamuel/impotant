2d14a3e6c8a50200fa286752516ecc8fa330569d
miorantsoa <miorantsioaRak@gmail.com>
2017-09-26 00:50:19 +0300

    Working project with header + history

--- src/app/screen/send/Send.js
+++ src/app/screen/send/Send.js
@@ -1,7 +1,11 @@
 //import liraries
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { StackNavigator } from "react-navigation";
+import { Icon } from "react-native-elements";
+import History from "../history/historyM";
+import headStyle from "../../styles/stylesC/headerStyle";
+import To from "./To";
 // create a component
 class Send extends Component {
   render() {
@@ -23,5 +27,56 @@ const styles = StyleSheet.create({
   }
 });
 
-//make this component available to the app
-export default Send;
+const NestedSendStack = StackNavigator({
+  Send: {
+    screen: Send,
+    navigationOptions: ({ navigation }) => ({
+      drawerIcon: ({ tintColor }) => (
+        <Icon name="ios-home-outline" size={25} type="ionicon" />
+      ),
+      title: "Home",
+      headerStyle: headStyle.headerBackground,
+      headerTitleStyle: headStyle.headerText,
+      headerTintColor: { color: "#fff" },
+      headerLeft: (
+        <View
+          style={{
+            alignContent: "center",
+            marginHorizontal: 10
+          }}
+        >
+          <TouchableOpacity onPress={() => navigation.navigate("DrawerOpen")}>
+            <Icon name="menu" size={30} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      ),
+      headerRight: (
+        <View
+          style={{
+            alignContent: "center",
+            marginRight: 10
+          }}
+        >
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("History", {
+                user_id: navigation.state.params.user_id
+              })}
+          >
+            <Icon name="back-in-time" type="entypo" size={30} color="#fff" />
+          </TouchableOpacity>
+        </View>
+      )
+    })
+  },
+  To: {
+    screen: To
+  },
+  History: {
+    screen: History,
+    navigationOptions: ({ navigation }) => ({
+      header: () => null
+    })
+  }
+});
+export default NestedSendStack;
