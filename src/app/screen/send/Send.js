3d9c8123c93a9d56508b1b61ed8cf23c9c1a3760
miorantsoa <miorantsioaRak@gmail.com>
2017-09-26 10:20:07 +0300

    Perform transaction

--- src/app/screen/send/Send.js
+++ src/app/screen/send/Send.js
@@ -6,10 +6,13 @@ import {
   StyleSheet,
   TouchableOpacity,
   Dimensions,
-  Platform
+  ActivityIndicator,
+  Platform,
+  ToastAndroid
 } from "react-native";
 import { StackNavigator } from "react-navigation";
 import { Icon, Button } from "react-native-elements";
+import Toast, { DURATION } from "react-native-easy-toast";
 import History from "../history/historyM";
 import headStyle from "../../styles/stylesC/headerStyle";
 import QrServices from "../../services/qrservices";
@@ -32,13 +35,17 @@ class Send extends Component {
       user_id: this.props.navigation.state.params.user_id,
       currency: "Ar",
       modal: null,
+      loading: true,
       isEditable: true
     };
   }
 
   async componentWillMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === "granted" });
+    this.setState({
+      hasCameraPermission: status === "granted",
+      loading: false
+    });
   }
 
   toggleFlash = () => {
@@ -51,9 +58,12 @@ class Send extends Component {
     }
   };
 
-  handleDoneEditing() {
-    console.log("Done editing");
-  }
+  handleDoneEditing = () => {
+    if (this.state.amount > 0 && this.state.user.length != 0) {
+      console.log("End Entering amount", this.state.user);
+      this.promptPin();
+    }
+  };
 
   removeModal() {
     this.setState({ modal: null });
@@ -63,16 +73,49 @@ class Send extends Component {
     this.setState({ amount: Services.formatNumber(text) });
   };
 
+  performTransaction() {
+    this.setState({ loading: true });
+    this.removeModal();
+    let services = new QrServices();
+    services
+      .performTransation(
+        this.state.amount,
+        this.state.user_id,
+        this.state.currency,
+        this.state.user,
+        ""
+      )
+      .then(rep => {
+        console.log(JSON.stringify(rep));
+        Platform.OS == "android"
+          ? ToastAndroid.show("Transaction success", ToastAndroid.SHORT)
+          : this.refs.toast.show("Transaction success", 1000);
+        this.setState({ loading: false });
+        this.initState();
+      });
+  }
+
+  handlePinInput = text => {
+    if (text.length === 4) {
+      console.log(text);
+      let services = new Services();
+      services.getData("pin").then(pin => {
+        if (pin === text) {
+          console.log("Ataovy le transaction");
+          this.performTransaction();
+        }
+      });
+    }
+  };
+
   promptPin() {
     this.setState({
       modal: (
         <PinModal
-          amount={this.state.amount}
+          amount={Services.formatNumber(this.state.amount)}
           user={this.state.user}
           currency={this.state.currency}
-          onChangeText={text => {
-            console.log(text);
-          }}
+          onChangeText={this.handlePinInput}
           onRequestClose={() => {
             this.removeModal();
           }}
@@ -97,7 +140,7 @@ class Send extends Component {
     });
   }
 
-  onResetAction = () => {
+  initState() {
     this.setState({
       amount: "",
       user: "",
@@ -105,6 +148,10 @@ class Send extends Component {
       currency: "Ar",
       isEditable: true
     });
+  }
+
+  onResetAction = () => {
+    this.initState();
   };
   promptInformation() {
     console.log("show info");
@@ -143,99 +190,113 @@ class Send extends Component {
     if (hasCameraPermission === null) {
       return <Text>Requesting for camera permission</Text>;
     } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
+      return <ActivityIndicator animating={true} />;
     } else {
       return (
         <View style={styles.container}>
-          <BarCodeScanner
-            onBarCodeRead={this._handleBarCodeRead}
-            torchMode={this.state.flashOn}
-            barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-            style={StyleSheet.absoluteFill}
-          />
-          <View
-            style={{
-              position: "absolute",
-              top: 0,
-              right: 0,
-              left: 0,
-              backgroundColor: "rgba(52, 73, 94,0.7)" //"#607D8B"
-            }}
-          >
-            <InputLeftIcon
-              iconName="expand-more"
-              onPress={() => {
-                console.log("Expand");
-                this.props.navigation.navigate("To", {
-                  onGoBack: data => {
-                    console.log(data);
-                    this.setState({ user: data });
-                  },
-                  user_id: this.state.user_id
-                });
-              }}
-              placeholder="Username"
-              onChangeText={user => this.setState({ user })}
-              value={this.state.user}
-              returnKeyType="none"
-              blurOnSubmit={false}
-            />
-            <InputLeftButton
-              buttonText={this.state.currency}
-              value={"" + this.state.amount}
-              placeholder="Amount"
-              keyboardType="numeric"
-              returnKeyType="done"
-              editable={this.state.isEditable}
-              onChangeText={amount =>
-                this.setState({ amount: Services.formatNumber(amount) })}
-              onEndEditing={() => {
-                this.handleDoneEditing();
-              }}
-            />
-          </View>
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              justifyContent: "space-around",
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              right: 0,
-              backgroundColor: "rgba(52, 73, 94,0.7)" //"#607D8B"
-            }}
-          >
-            <Button
-              buttonStyle={styles.controlButton}
-              icon={{ name: "clear-all", size: 25 }}
-              onPress={this.onResetAction}
-            >
-              <Icon name="clear-all" size={25} color="#fafafa" />
-            </Button>
-            <Button
-              buttonStyle={styles.controlButton}
-              icon={{ name: this.state.flashIcon, size: 25 }}
-              onPress={this.toggleFlash}
-            >
-              <Icon name={this.state.flashIcon} size={25} color="#fafafa" />
-            </Button>
-            {/* <Button
+          {this.state.loading ? (
+            <ActivityIndicator animating={true} />
+          ) : (
+            <View style={{ flex: 1 }}>
+              <BarCodeScanner
+                onBarCodeRead={this._handleBarCodeRead}
+                torchMode={this.state.flashOn}
+                barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
+                style={StyleSheet.absoluteFill}
+              />
+              <View
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  right: 0,
+                  left: 0,
+                  backgroundColor: "rgba(52, 73, 94,0.7)" //"#607D8B"
+                }}
+              >
+                <InputLeftIcon
+                  iconName="expand-more"
+                  onPress={() => {
+                    console.log("Expand");
+                    this.props.navigation.navigate("To", {
+                      onGoBack: data => {
+                        console.log(data);
+                        this.setState({ user: data });
+                      },
+                      user_id: this.state.user_id
+                    });
+                  }}
+                  placeholder="Username"
+                  onChangeText={user => this.setState({ user })}
+                  value={this.state.user}
+                  returnKeyType="none"
+                  blurOnSubmit={false}
+                />
+                <InputLeftButton
+                  buttonText={this.state.currency}
+                  value={"" + this.state.amount}
+                  placeholder="Amount"
+                  keyboardType="numeric"
+                  returnKeyType="done"
+                  editable={this.state.isEditable}
+                  onChangeText={amount =>
+                    this.setState({ amount: Services.formatNumber(amount) })}
+                  onEndEditing={this.handleDoneEditing}
+                />
+              </View>
+              <View
+                style={{
+                  flex: 1,
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  backgroundColor: "rgba(52, 73, 94,0.7)" //"#607D8B"
+                }}
+              >
+                <Button
+                  buttonStyle={styles.controlButton}
+                  icon={{ name: "clear-all", size: 25 }}
+                  onPress={this.onResetAction}
+                >
+                  <Icon name="clear-all" size={25} color="#fafafa" />
+                </Button>
+                <Button
+                  buttonStyle={styles.controlButton}
+                  icon={{ name: this.state.flashIcon, size: 25 }}
+                  onPress={this.toggleFlash}
+                >
+                  <Icon name={this.state.flashIcon} size={25} color="#fafafa" />
+                </Button>
+                {/* <Button
               buttonStyle={styles.controlButton}
               onPress={this.promptInformation}
               icon={{ name: "info", size: 25 }}
             >
               <Icon name="info" size={25} color="#fafafa" />
             </Button> */}
-            <Button
-              buttonStyle={styles.controlButton}
-              icon={{ name: "send", size: 25 }}
-              onPress={this.onContinueAction}
-            >
-              <Icon name="send" size={25} color="#fafafa" />
-            </Button>
-          </View>
+                <Button
+                  buttonStyle={styles.controlButton}
+                  icon={{ name: "send", size: 25 }}
+                  onPress={this.onContinueAction}
+                >
+                  <Icon name="send" size={25} color="#fafafa" />
+                </Button>
+              </View>
+            </View>
+          )}
           {this.state.modal}
+          {Platform.OS == "ios" ? (
+            <Toast
+              ref="toast"
+              position="top"
+              positionValue={20}
+              fadeInDuration={750}
+              fadeOutDuration={1000}
+              opacity={0.8}
+            />
+          ) : null}
         </View>
       );
     }
