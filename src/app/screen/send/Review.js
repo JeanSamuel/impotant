318ac5572de3223b661501d81fb680726129e8d1
miorantsoa <miorantsioaRak@gmail.com>
2017-10-18 11:35:01 +0300

    Review

--- src/app/screen/send/Review.js
+++ src/app/screen/send/Review.js
@@ -9,22 +9,60 @@ import {
   TouchableHighlight
 } from "react-native";
 import { Icon } from "react-native-elements";
+import QrServices from "../../services/qrservices";
 
 // create a component
 const { height, width } = Dimensions.get("window");
 class Review extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_id: this.props.navigation.state.params.user_id,
+      amount: this.props.navigation.state.params.amount,
+      user: this.props.navigation.state.params.user,
+      currency: "Ar"
+    };
+  }
+
+  _handleContinue() {
+    services = new QrServices();
+    let services = new QrServices();
+    services
+      .performTransation(
+        this.state.amount,
+        this.state.user_id,
+        this.state.currency,
+        this.state.user,
+        ""
+      )
+      .then(rep => {
+        alert("succées");
+      });
+  }
   render() {
     return (
       <View style={styles.container}>
         <ScrollView>
           <View style={styles.reviewBox}>
-            <Text>
-              {this.props.navigation.state.params.user}{" "}
-              {this.props.navigation.state.params.amount}{" "}
-            </Text>
-          </View>
-          <View style={styles.informationBox}>
-            <Text>OK</Text>
+            <View style={{ flexDirection: "row" }}>
+              <Text style={styles.amountReview}>{this.state.amount}</Text>
+              <Text style={{ fontSize: 35, marginBottom: 30 }}>
+                {this.state.currency}
+              </Text>
+            </View>
+            <View style={styles.informationBox}>
+              <Text style={{ textAlign: "center", fontWeight: "bold" }}>
+                Vous allez envoyer {this.state.amount} {this.state.currency} à{" "}
+                {this.state.user}
+              </Text>
+              <View style={{ marginTop: 10 }}>
+                <Text style={{ textAlign: "center", color: "#797979" }}>
+                  L'argent sera envoyer dans le compte Ariary.net de la
+                  personne. La personne recevra un email, ou une notification à
+                  propos de la transaction.
+                </Text>
+              </View>
+            </View>
           </View>
         </ScrollView>
 
@@ -33,8 +71,7 @@ class Review extends Component {
             justifyContent: "center",
             backgroundColor: "#193441",
             height: 50,
-            width: width,
-            flexDirection: "row"
+            width: width
           }}
           onPress={() => {
             this._handleContinue();
@@ -51,7 +88,6 @@ class Review extends Component {
             >
               Envoyer maintenant
             </Text>
-            <Icon name="navigate-next" size={30} color="#fff" />
           </View>
         </TouchableHighlight>
       </View>
@@ -69,12 +105,25 @@ const styles = StyleSheet.create({
   },
   reviewBox: {
     backgroundColor: "#fff",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 3,
+    borderBottomRightRadius: 3,
+    paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
     alignSelf: "center",
-    height: height / 2 - 50,
-    width: width - 50
+    height: height / 2,
+    width: width - 30
   },
   informationBox: {
-    width: width
+    width: width,
+    marginTop: 20,
+    paddingHorizontal: 25
+  },
+  amountReview: {
+    textAlign: "center",
+    fontSize: 75
   }
 });
 
